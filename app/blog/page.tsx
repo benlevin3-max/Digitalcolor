b8bdcb534ee51f5eb628df12d4a8af6ad98732f3
Ben Levin <benlevin3@gmail.com>
2026-04-23 10:58:17 +0300

    fix blog script loading

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+import { useEffect } from 'react';
 export default function BlogPage() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://app.trysoro.com/api/embed/f0818072-33ab-41f0-bc05-6196867c4602';
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => { document.body.removeChild(script); };
+  }, []);
   return (
-    <main>
+    <main style={{ minHeight: '100vh', padding: '40px 20px' }}>
       <div id="soro-blog"></div>
-      <script src="https://app.trysoro.com/api/embed/f0818072-33ab-41f0-bc05-6196867c4602" defer></script>
     </main>
   );
 }
