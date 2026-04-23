d1656a5960e21c47cf55adff0f322b255a105d8a
Ben Levin <benlevin3@gmail.com>
2026-04-23 10:47:56 +0300

    add blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <main>
+      <div id="soro-blog"></div>
+      <script src="https://app.trysoro.com/api/embed/f0818072-33ab-41f0-bc05-6196867c4602" defer></script>
+    </main>
+  );
+}
